4cfa01983f21b5bf8e7e41ef9056fa6c6738c88d
smalldrew <idk.contact1000@gmail.com>
2024-02-06 13:38:23 -0800

    feat: scraping github contribution graph

---
+++ github_scraper.py
@@ -0,0 +1,45 @@
+# This version is probably better than the API method
+import requests
+import util.SMS as SMS
+from datetime import date
+from bs4 import BeautifulSoup
+
+
+USERNAME = 'smalldrew'  # put your username here
+
+def check_code_today(username: str) -> bool:
+    """
+    Check if a user with the given username has made any commits on GitHub today.
+    
+     Parameters:
+        username (str): The GitHub username to check.
+
+    Returns:
+        bool: True if the user has made commits today, False otherwise. 
+        Returns None if there is an issue with the request to GitHub.
+    """
+
+    url = f'https://github.com/{username}' 
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        web_scrape = BeautifulSoup(response.content, 'html.parser')
+        commit_box = web_scrape.find('td', {'data-date': str(date.today())})      
+        data_level = commit_box['data-level']
+
+        return False if data_level == '0' else True
+    return None
+
+
+def check(username: str) -> None:
+    """Sends a message if you didn't code. Otherwise, it sends a good job message."""
+    if check_code_today(username):
+        print('LOG: Sent good job message.')
+        SMS.send_text('Good job. You coded today.')
+    else:
+        print('LOG: Sent angry message.')
+        SMS.send_text('GO CODE RIGHT NOW OR YOU WILL DIE.')
+
+
+if __name__ == '__main__':
+    check(USERNAME)
